e96bd7acd52996e8c16b0bcc52dcabac35fd0ed8
saintmattry <saintmattry@gmail.com>
2026-09-11 03:12:53 +0100

    fix: hardcode prod API

--- src/api.js
+++ src/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://blog-vault-backend-1.onrender.com/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  headers: { 'Content-Type': 'application/json' }
 });
 
 api.interceptors.request.use((config) => {
