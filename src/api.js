9efb90dc4d58fa41722a9964abfcda482db34689
saintmattry <saintmattry@gmail.com>
2026-09-11 03:02:07 +0100

    fix: hardcode production API URL

--- src/api.js
+++ src/api.js
@@ -1,32 +1,10 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// });
-
-// // Automatically attach token to every request
-// api.interceptors.request.use((config) => {
-//   const user = JSON.parse(localStorage.getItem('user') || 'null');
-//   if (user?.token) {
-//     config.headers.Authorization = `Bearer ${user.token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
-
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-console.log("USING API URL:", apiUrl); // check in F12
-
 const api = axios.create({
-  baseURL: apiUrl,
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: 'https://blog-vault-backend-1.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 api.interceptors.request.use((config) => {
